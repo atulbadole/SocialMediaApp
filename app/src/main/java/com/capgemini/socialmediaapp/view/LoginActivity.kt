89c654cca6ef3aadbfbd0de87b0c6fd3e013b1dc
Krishsaluja69 <krishsaluja69@gmail.com>
2024-02-09 10:55:47 +0530

    Navigation b/w Login and Signup

--- app/src/main/java/com/capgemini/socialmediaapp/view/LoginActivity.kt
+++ app/src/main/java/com/capgemini/socialmediaapp/view/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.capgemini.socialmediaapp.view
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.capgemini.socialmediaapp.R
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var  SignUpButton : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        SignUpButton = findViewById(R.id.l_Signup)
+
+        SignUpButton.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
--- app/src/main/java/com/capgemini/socialmediaapp/view/SignUpActivity.kt
+++ app/src/main/java/com/capgemini/socialmediaapp/view/SignUpActivity.kt
@@ -1,12 +1,43 @@
 package com.capgemini.socialmediaapp.view
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.capgemini.socialmediaapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var BackButton : FloatingActionButton
+    lateinit var SignIn : Button
+    lateinit var LoginButton : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        BackButton = findViewById(R.id.s_Backbutton)
+        SignIn = findViewById(R.id.s_SignUp)
+        LoginButton = findViewById(R.id.s_login)
+
+        BackButton.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        SignIn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        LoginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
